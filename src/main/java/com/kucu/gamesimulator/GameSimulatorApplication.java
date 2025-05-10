ad3abe0ee70270bc9d8dda720e49f50f452c9b6e
kucu <kacper.kujawa98@gmail.com>
2025-05-10 19:12:00 +0200

    Initial commit

---
+++ src/main/java/com/kucu/gamesimulator/GameSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.kucu.gamesimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kucu/gamesimulator/GameSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kucu.gamesimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
