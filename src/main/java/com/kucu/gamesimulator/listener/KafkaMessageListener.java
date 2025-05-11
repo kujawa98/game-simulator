98dc43a9cc323a112afd122ee1743304d23f026b
kucu <kacper.kujawa98@gmail.com>
2025-05-11 16:22:30 +0200

    Set static final variables

--- src/main/java/com/kucu/gamesimulator/listener/KafkaMessageListener.java
+++ src/main/java/com/kucu/gamesimulator/listener/KafkaMessageListener.java
@@ -3,7 +3,11 @@ package com.kucu.gamesimulator.listener;
 import org.springframework.kafka.annotation.KafkaListener;
 
 public class KafkaMessageListener {
-    @KafkaListener(topics = "test", groupId = "game-sim")
+    private static final String GROUP_ID = "game-sim";
+    private static final String TEST_TOPIC = "test";
+
+
+    @KafkaListener(topics = TEST_TOPIC, groupId = GROUP_ID)
     public void listen(String message) {
         System.out.println("Received: " + message);
     }
